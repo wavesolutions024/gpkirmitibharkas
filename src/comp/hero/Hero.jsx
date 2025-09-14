38fdd92339dcec08fd68e2a642d169fefa7ba329
rishabhkhade <rishabhkhade95@gmail.com>
2025-09-14 22:57:09 +0530

    pp

--- src/comp/hero/Hero.jsx
+++ src/comp/hero/Hero.jsx
@@ -1,28 +1,31 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Hero.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-
 import { Navigation } from "swiper/modules";
 
 const Hero = () => {
-//   useEffect(() => {
-//     AOS.init({ duration: 1000, easing: "ease-in-out" });
-//   }, []);
+
+
+const data = [
+  "Breaking News: Event 1",
+  "Update: Event 2",
+  "Alert: Event 3"
+];
+
+const marqueeText = data.join("  |  ");
+
+
   return (
-    <>
-      <div className="parent hero-parent">
-        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-        </Swiper>
+    <div className="parent hero-parent bg-img-cover">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide></SwiperSlide>
+      </Swiper>
+      <div className="custom-marquee">
+        <span className="marquee-text">{marqueeText}</span>
       </div>
-    </>
+    </div>
   );
 };
 
