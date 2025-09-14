faad213e1500933341116859224acde4f3ddb037
ketanrd26 <ketandudka566123@gmail.com>
2025-09-14 22:07:44 +0530

    push

---
+++ src/comp/leaders/Leaders.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import "./Leaders.scss";
+import fd from "../../assets/fd.png";
+const Leaders = () => {
+  return (
+    <>
+      <div class="leaders_parent parent">
+        <div class="leader_cont cont">
+          <div class="card">
+            <div class="top_section">
+              <div class="image">
+                <img src={fd} alt="" />
+              </div>
+            </div>
+            <div class="bottom_section">
+              <div class="label">Hon'ble Chief Minister</div>
+              <h3 class="name">Shri. Devendra Fadnavis</h3>
+              <p class="para">
+                The Rural Development Department was established on May 1, 1960.
+                The Rural Development Department of the Government of
+                Maharashtra is working for the all-round development of rural
+                areas.
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="top_section">
+              <div class="image">
+                <img src={fd} alt="" />
+              </div>
+            </div>
+            <div class="bottom_section">
+              <div class="label">Hon'ble Chief Minister</div>
+              <h3 class="name">Shri. Devendra Fadnavis</h3>
+              <p class="para">
+                The Rural Development Department was established on May 1, 1960.
+                The Rural Development Department of the Government of
+                Maharashtra is working for the all-round development of rural
+                areas.
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="top_section">
+              <div class="image">
+                <img src={fd} alt="" />
+              </div>
+            </div>
+            <div class="bottom_section">
+              <div class="label">Hon'ble Chief Minister</div>
+              <h3 class="name">Shri. Devendra Fadnavis</h3>
+              <p class="para">
+                The Rural Development Department was established on May 1, 1960.
+                The Rural Development Department of the Government of
+                Maharashtra is working for the all-round development of rural
+                areas.
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="top_section">
+              <div class="image">
+                <img src={fd} alt="" />
+              </div>
+            </div>
+            <div class="bottom_section">
+              <div class="label">Hon'ble Chief Minister</div>
+              <h3 class="name">Shri. Devendra Fadnavis</h3>
+              <p class="para">
+                The Rural Development Department was established on May 1, 1960.
+                The Rural Development Department of the Government of
+                Maharashtra is working for the all-round development of rural
+                areas.
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="top_section">
+              <div class="image">
+                <img src={fd} alt="" />
+              </div>
+            </div>
+            <div class="bottom_section">
+              <div class="label">Hon'ble Chief Minister</div>
+              <h3 class="name">Shri. Devendra Fadnavis</h3>
+              <p class="para">
+                The Rural Development Department was established on May 1, 1960.
+                The Rural Development Department of the Government of
+                Maharashtra is working for the all-round development of rural
+                areas.
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="top_section">
+              <div class="image">
+                <img src={fd} alt="" />
+              </div>
+            </div>
+            <div class="bottom_section">
+              <div class="label">Hon'ble Chief Minister</div>
+              <h3 class="name">Shri. Devendra Fadnavis</h3>
+              <p class="para">
+                The Rural Development Department was established on May 1, 1960.
+                The Rural Development Department of the Government of
+                Maharashtra is working for the all-round development of rural
+                areas.
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="top_section">
+              <div class="image">
+                <img src={fd} alt="" />
+              </div>
+            </div>
+            <div class="bottom_section">
+              <div class="label">Hon'ble Chief Minister</div>
+              <h3 class="name">Shri. Devendra Fadnavis</h3>
+              <p class="para">
+                The Rural Development Department was established on May 1, 1960.
+                The Rural Development Department of the Government of
+                Maharashtra is working for the all-round development of rural
+                areas.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Leaders;
