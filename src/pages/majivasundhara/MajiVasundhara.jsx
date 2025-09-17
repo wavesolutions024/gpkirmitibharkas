9ff7cdd943768009f0aede62b2fe4d0a4a43b95c
Ketan Dudka <ketandudka566123@gmail.com>
2025-09-17 12:20:58 +0530

    push

--- src/pages/majivasundhara/MajiVasundhara.jsx
+++ src/pages/majivasundhara/MajiVasundhara.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import "./MajiVasundhara.scss";
+import ReactPlayer from "react-player";
+
+import clean1 from "../../assets/gallery/clean/clean1.mp4"
 const MajiVasundhara = () => {
   const [active, setActive] = useState("Videos");
+  const [data, setData] = useState("स्वच्छ सर्व्हेक्षण ग्रामीण 2025");
+
   return (
     <>
       <div class="maji_vasundhara_parent parent">
@@ -21,7 +26,32 @@ const MajiVasundhara = () => {
             </div>
           </div>
           <div class="bottom_section">
-            <div class="card"></div>
+            <div class="folders">
+              <div
+                class={
+                  data === "स्वच्छ सर्व्हेक्षण ग्रामीण 2025"
+                    ? "top_btn active"
+                    : "top_btn"
+                }
+                onClick={() => setData("स्वच्छ सर्व्हेक्षण ग्रामीण 2025")}
+              >
+                स्वच्छ सर्व्हेक्षण ग्रामीण 2025
+              </div>
+            </div>
+
+            {data === "स्वच्छ सर्व्हेक्षण ग्रामीण 2025" && (
+              <div class="data">
+                <div class="card">
+                  <ReactPlayer
+                    url={clean1}
+                    height="100%"
+                    width="100%"
+                    className="react-video"
+                    controls={true}
+                  />
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
